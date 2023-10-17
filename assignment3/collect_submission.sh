5857d4eeb0bf69ba8baab7c6eb6773c9c1e69699
Michael Fox <mjfox314@gmail.com>
2023-10-17 09:53:14 -0400

    modified: collect_submission.sh

--- assignment3/collect_submission.sh
+++ assignment3/collect_submission.sh
@@ -1,2 +1,2 @@
 rm -f assignment_3_submission.zip
-zip -r assignment_3_submission.zip *.py style_modules/*.py visualizers/*py
+zip -r assignment_3_submission.zip *.py style_modules/*.py visualizers/*py data/*.npz
